35082d48b93896f184fe838f212c03cdd2fc5be9
Varun Raghav Ramesh <vrramesh@0587363391.wireless.umich.net>
2015-01-17 05:33:09 -0500

    Contains the login with LinkedIn

--- app/src/main/java/randhawa/deep/faceflash/LoginActivity.java
+++ app/src/main/java/randhawa/deep/faceflash/LoginActivity.java
@@ -2,38 +2,77 @@ package randhawa.deep.faceflash;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import org.brickred.socialauth.Profile;
+import org.brickred.socialauth.android.DialogListener;
+import org.brickred.socialauth.android.SocialAuthAdapter;
+import org.brickred.socialauth.android.SocialAuthError;
+import org.brickred.socialauth.android.SocialAuthListener;
 
 
 public class LoginActivity extends ActionBarActivity {
 
+    SocialAuthAdapter adapter;
+    Button linkedIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        adapter = new SocialAuthAdapter(new ResponseListener());
+        linkedIn = (Button) findViewById(R.id.linkedin);
+        linkedIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adapter.authorize(LoginActivity.this,
+                        SocialAuthAdapter.Provider.LINKEDIN);
+            }
+        });
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    // Calls the linkedIn API.
+    public class ResponseListener implements DialogListener {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        @Override
+        public void onComplete(Bundle values){
+           // System.out.println("Hi");
+           adapter.getUserProfileAsync(new ProfileDataListener());
+        }
+        @Override
+        public void onCancel(){
+            System.out.println("Operation was cancelled");
+        }
+        @Override
+        public void onBack(){
+            System.out.println("Operation was cancelled");
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public void onError(SocialAuthError socialAuthError) {
+            System.out.println(socialAuthError.getMessage());
+        }
     }
+
+       public class ProfileDataListener implements SocialAuthListener<Profile> {
+
+           @Override
+           public void onExecute(String name, Profile profile){
+               Log.d("Custom-UI", "Receiving Data");
+               Profile profilemap = profile;
+               System.out.println(profilemap.getFirstName() + " " +
+                       profilemap.getLastName());
+               //System.out.println(profilemap.getGender());
+           }
+
+           @Override
+           public void onError(SocialAuthError socialAuthError) {
+               System.out.println(socialAuthError.getMessage());
+           }
+       }
+
 }
